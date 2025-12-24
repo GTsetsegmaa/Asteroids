ee3ef69c77388718ab93595c992d3d1fbb213924
GTsetsegmaa <gtsetsegm@gmail.com>
2025-12-23 20:55:27 -0800

    add movement to player

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -22,7 +29,12 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
