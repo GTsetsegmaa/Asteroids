85a37ae5dc99289250ce31f23fc90705a39e6528
George Tsetsegmaa <macdev@macdev-2.local>
2025-12-24 19:51:57 -0800

    add asteroid and asteroidfield

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 import pygame
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
